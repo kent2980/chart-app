1ecd51997e127753297532ce246370e754162cb1
kent2980 <kent2980@gmail.com>
2023-12-08 23:48:35 +0900

    commit

--- src/services/FsstockApiServies.ts
+++ src/services/FsstockApiServies.ts
@@ -45,6 +45,123 @@ const callApi = async (path: string, params: Record<string, any>): Promise<any> 
     }
 };
 
+export interface ExplainListParamsInterface {
+
+    /**
+     * 報告書ID
+     * @type {string}
+     */
+    id: string;
+
+    /**
+     * 銘柄コード
+     * @type {string}
+     */
+    code: string;
+
+    /**
+     * 公表日
+     * @type {string}
+     */
+    publication_date: string;
+
+    /**
+     * 報告区分
+     * @type {string}
+     */
+    report_cat: string;
+
+    /**
+     * インデックスID
+     * @type {string}
+     */
+    index_id: string;
+
+    /**
+     * 会計期間
+     * @type {string}
+     */
+    period: string;
+
+    /**
+     * 会計年度
+     * @type {string}
+     */
+    period_year: string;
+
+    /**
+     * (指定)IDより新しい報告書
+     * @type {string}
+     */
+    index_id_gt: string;
+
+    /**
+     * (指定)ID以前の報告書
+     * @type {string}
+     */
+    index_id_lte: string;
+
+    /**
+     * ソート順
+     * @type {string}
+     */
+    ordering: string;
+}
+
+export class ExplainList implements ExplainListParamsInterface {
+
+    id: string = "";
+    code: string = "";
+    publication_date: string = "";
+    report_cat: string = "";
+    index_id: string = "";
+    period: string = "";
+    period_year: string = "";
+    index_id_gt: string = "";
+    index_id_lte: string = "";
+    ordering: string = "";
+
+    /**
+     * パラメータオブジェクトを取得します。
+     * @returns {ExplainListParamsInterface}
+     */
+    get Record(): ExplainListParamsInterface {
+        return {
+            id: this.id,
+            code: this.code,
+            publication_date: this.publication_date,
+            report_cat: this.report_cat,
+            index_id: this.index_id,
+            period: this.period,
+            period_year: this.period_year,
+            index_id_gt: this.index_id_gt,
+            index_id_lte: this.index_id_lte,
+            ordering: this.ordering
+        }
+    }
+
+    /**
+     * 他のExplainListオブジェクトと等しいかどうかをチェックします。
+     * @param {ExplainList} other - 比較するオブジェクト
+     * @returns {boolean} - オブジェクトが等しい場合はtrue、それ以外はfalse
+     */
+    equals(other: ExplainList): boolean {
+        return (
+            this.id === other.id &&
+            this.code === other.code &&
+            this.publication_date === other.publication_date &&
+            this.report_cat === other.report_cat &&
+            this.index_id === other.index_id &&
+            this.period === other.period &&
+            this.period_year === other.period_year &&
+            this.index_id_gt === other.index_id_gt &&
+            this.index_id_lte === other.index_id_lte &&
+            this.ordering === other.ordering
+        );
+    }
+}
+
+
 /**
  * explain APIを呼び出す関数
  * @param params - クエリパラメータのオブジェクト
@@ -56,12 +173,12 @@ export const ExplainListApi = {
      * @param params - クエリパラメータのオブジェクト
      * @returns explain APIのレスポンスのデータ
      */
-    fetchData: async (params: Record<string, any>): Promise<any> => {
+    fetchData: async (params: ExplainListParamsInterface): Promise<any> => {
         return await callApi('explain', params);
     },
 };
 
-type ExplainItemParamsType = {
+interface ExplainItemParamsInterface {
     calLinkFromElement: string;
     calLinkNull: string;
     calLinkOrder: string;
@@ -81,49 +198,79 @@ type ExplainItemParamsType = {
 }
 
 // ExplainItemParamsクラスの定義
-export class ExplainItemParams {
-    /**
-     * @type {string} - calLinkFromElement
-     */
-    calLinkFromElement: string = "";
-
-    /**
-     * @type {string} - calLinkNull
-     */
-    calLinkNull: string = "";
-
-    /**
-     * @type {string} - calLinkOrder
-     */
-    calLinkOrder: string = "";
-
-    code:string = "";
-
-    defLinkFromElement:string = "";
+export class ExplainItemParams implements ExplainItemParamsInterface {
+    calLinkFromElement: string = '';
+    calLinkNull: string = '';
+    calLinkOrder: string = '';
+    code: string = '';
+    defLinkFromElement: string = '';
+    defLinkOrder: string = '';
+    doc_element: string = '';
+    element: string = '';
+    elementLabel: string = '';
+    explain: string = '';
+    financial_statement: string = '';
+    page: string = '';
+    preLinkFromElement: string = '';
+    preLinkOrder: string = '';
+    publication_date: string = '';
+    report_detail_cat: string = '';
 
     /**
      * パラメータオブジェクトを取得
-     * @returns {ExplainItemParamsType}
+     * @returns {ExplainItemParamsInterface}
      */
-    get Record(): any {
+    get Record(): ExplainItemParamsInterface {
         return {
             calLinkFromElement: this.calLinkFromElement,
             calLinkNull: this.calLinkNull,
             calLinkOrder: this.calLinkOrder,
-            // ... (other properties)
+            code: this.code,
+            defLinkFromElement: this.defLinkFromElement,
+            defLinkOrder: this.defLinkOrder,
+            doc_element: this.doc_element,
+            element: this.element,
+            elementLabel: this.elementLabel,
+            explain: this.explain,
+            financial_statement: this.financial_statement,
+            page: this.page,
+            preLinkFromElement: this.preLinkFromElement,
+            preLinkOrder: this.preLinkOrder,
+            publication_date: this.publication_date,
+            report_detail_cat: this.report_detail_cat,
         };
     }
 
+
     /**
-     * 他のExplainItemParamsとの等価性を判定
-     * @param {ExplainItemParams} other - 比較対象のExplainItemParams
-     * @returns {boolean} - 等しい場合はtrue、そうでない場合はfalse
-     */
-    equals(other: ExplainItemParams): boolean {
-        return(
-            this.calLinkFromElement===other.calLinkFromElement
-        )
+    * 他のオブジェクトと等しいかどうかをチェックする
+    * @param {*} obj - 比較対象のオブジェクト
+    * @returns {boolean} - 等しい場合はtrue、そうでない場合はfalse
+    */
+    equals(obj: any): boolean {
+        if (this === obj) {
+            return true;
+        }
+        return (
+            this.calLinkFromElement === obj.calLinkFromElement &&
+            this.calLinkNull === obj.calLinkNull &&
+            this.calLinkOrder === obj.calLinkOrder &&
+            this.code === obj.code &&
+            this.defLinkFromElement === obj.defLinkFromElement &&
+            this.defLinkOrder === obj.defLinkOrder &&
+            this.doc_element === obj.doc_element &&
+            this.element === obj.element &&
+            this.elementLabel === obj.elementLabel &&
+            this.explain === obj.explain &&
+            this.financial_statement === obj.financial_statement &&
+            this.page === obj.page &&
+            this.preLinkFromElement === obj.preLinkFromElement &&
+            this.preLinkOrder === obj.preLinkOrder &&
+            this.publication_date === obj.publication_date &&
+            this.report_detail_cat === obj.report_detail_cat
+        );
     }
+
 }
 
 /**
@@ -137,13 +284,13 @@ export const ExplainItemApi = {
      * @param params - クエリパラメータのオブジェクト
      * @returns item APIのレスポンスのデータ
      */
-    fetchData: async (params: ExplainItemParamsType): Promise<any> => {
+    fetchData: async (params: ExplainItemParamsInterface): Promise<any> => {
         return await callApi('item', params);
     },
 };
 
 
-type StockBrandsParamsType = {
+interface StockBrandsParamsInterface {
     code: string;
     market_product_code: string;
     number_33_industry_code: string;
@@ -152,7 +299,7 @@ type StockBrandsParamsType = {
 }
 
 // StockBrandsParamsクラスの定義
-export class StockBrandsParams {
+export class StockBrandsParams implements StockBrandsParamsInterface {
     /**
      * @type {string} - 銘柄コード
      */
@@ -180,9 +327,9 @@ export class StockBrandsParams {
 
     /**
      * パラメータオブジェクトを取得
-     * @returns {StockBrandsParamsType}
+     * @returns {StockBrandsParamsInterface}
      */
-    get Record(): StockBrandsParamsType {
+    get Record(): StockBrandsParamsInterface {
         return {
             code: this.code,
             market_product_code: this.market_product_code,
@@ -219,12 +366,12 @@ export const StockBrandsApi = {
      * @param params - クエリパラメータのオブジェクト
      * @returns brands APIのレスポンスのデータ
      */
-    fetchData: async (params: StockBrandsParamsType): Promise<any> => {
+    fetchData: async (params: StockBrandsParamsInterface): Promise<any> => {
         return await callApi('brands', params);
     },
 };
 
-type StockChartParamsType = {
+interface StockChartParamsInterface {
     /**
      * 銘柄コード
      */
@@ -237,7 +384,7 @@ type StockChartParamsType = {
 /**
 * 株価データ取得のためのパラメータクラス
 */
-export class StockChartParams {
+export class StockChartParams implements StockChartParamsInterface {
     /**
      * @type {string} - 銘柄コード
      */
@@ -260,14 +407,14 @@ export class StockChartParams {
 
     /**
      * パラメータオブジェクトを取得
-     * @type {StockChartParamsType}
+     * @type {StockChartParamsInterface}
      */
-    get Record(): StockChartParamsType {
+    get Record(): StockChartParamsInterface {
         return {
             code: this.code,
-            date_range_gte:this.date_range_gte,
-            date_range_lte:this.date_range_lte,
-            select_date:this.select_date,
+            date_range_gte: this.date_range_gte,
+            date_range_lte: this.date_range_lte,
+            select_date: this.select_date,
         };
     }
 
@@ -298,7 +445,7 @@ export const StockChartDataApi = {
      * @param params - クエリパラメータのオブジェクト
      * @returns result APIのレスポンスのデータ
      */
-    fetchData: async (params: StockChartParamsType): Promise<any> => {
+    fetchData: async (params: StockChartParamsInterface): Promise<any> => {
         return await callApi('result', params);
     },
 };
